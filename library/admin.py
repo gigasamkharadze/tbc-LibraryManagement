2ebcc4b402ec6580dee391b144e23673fe576a48
gigasamkharadze <samkharadze.giga@kiu.edu.ge>
2024-05-14 20:00:21 +0400

    feat: add debug toolbar and optimize queries

--- library/admin.py
+++ library/admin.py
@@ -9,39 +9,44 @@ from library.filters import QuantityFilter
 class GenreAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
     search_fields = ('name', 'description')
-    list_filter = ('name',)
-    ordering = ('name',)
 
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
     search_fields = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
-    list_filter = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    list_filter = ('date_of_birth', 'date_of_death')
     ordering = ('last_name', 'first_name', 'date_of_birth')
 
+
+from django.contrib import admin
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre', 'publication_date', 'quantity', 'total_borrowed',
                     'reservations_count', 'currently_borrowed')
     search_fields = ('title', 'author__first_name', 'author__last_name', 'genre__name', 'publication_date', 'quantity')
-    list_filter = (QuantityFilter, 'genre', 'publication_date')
+    list_filter = ('genre', 'publication_date')
     ordering = ('title', 'publication_date', 'author', 'quantity')
     list_per_page = 100
 
-    def display_genre(self, book: Book):
+    def get_queryset(self, request):
+        return (super().get_queryset(request)
+                .select_related('author')
+                .prefetch_related('genre', 'transactions', 'reservations'))
+
+    def display_genre(self, book):
         return ", ".join([genre.name for genre in book.genre.all()])
     display_genre.short_description = 'Genre'
 
-    def total_borrowed(self, book: Book):
+    def total_borrowed(self, book):
         return book.transactions.count()
-    total_borrowed.short_description = 'total borrowed'
+    total_borrowed.short_description = 'Total Borrowed'
 
-    def reservations_count(self, book: Book):
+    def reservations_count(self, book):
         return book.reservations.count()
-    reservations_count.short_description = 'reserved'
+    reservations_count.short_description = 'Reserved'
 
-    def currently_borrowed(self, book: Book):
+    def currently_borrowed(self, book):
         return book.transactions.filter(return_date__isnull=True).count()
-    currently_borrowed.short_description = 'currently borrowed'
+    currently_borrowed.short_description = 'Currently Borrowed'
--- lms/settings.py
+++ lms/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'library',
     'users',
     'transactions',
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'lms.urls'
@@ -114,6 +116,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 LANGUAGE_CODE = 'en-us'
 
