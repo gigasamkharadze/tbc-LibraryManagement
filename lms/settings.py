e725a67af348a9936c31028b04124ae5b44fe626
gigasamkharadze <samkharadze.giga@kiu.edu.ge>
2024-05-14 23:35:20 +0400

    feat: swagger UI

--- lms/settings.py
+++ lms/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'debug_toolbar',
     'library',
     'users',
@@ -140,3 +141,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'Basic': {
+            'type': 'basic'
+        }
+    }
+}
