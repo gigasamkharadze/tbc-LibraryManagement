0542b7a15ce3b3a91e2dd237c5d22126bc2facd2
gigasamkharadze <samkharadze.giga@kiu.edu.ge>
2024-05-18 22:08:58 +0400

    feat: login view

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,8 @@
-from frontend.views import home
 from django.urls import path
+from frontend.views import home, login_view
+
 
 urlpatterns = [
     path('', home, name='home'),
+    path('login/', login_view, name='login'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+import requests
+from django.http import JsonResponse
 
 
 def home(request):
     return render(request, 'frontend/home.html', {
         'user': request.user,
     })
+
+
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        response = requests.post('http://localhost:8000/api/token/', data={
+            'email': email,
+            'password': password,
+        })
+        if response.status_code == 200:
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                request.session['refresh'] = response.json()['refresh']
+                request.session['access'] = response.json()['access']
+                return redirect('home')
+        return render(request, 'frontend/login.html', {
+            'error': 'Invalid username or password',
+        })
+    return render(request, 'frontend/login.html')
