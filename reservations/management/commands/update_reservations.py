b7f84589e659f5a381f2f7c2e4110e7f28cbb0c8
gigasamkharadze <samkharadze.giga@kiu.edu.ge>
2024-05-20 23:36:28 +0400

    bug: decrease quantity of the book when reserved, increase when reservation cancelled

--- reservations/management/commands/update_reservations.py
+++ reservations/management/commands/update_reservations.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from datetime import timedelta
 from reservations.models import Reservation
 from reservations.choices import StatusChoices
+from django.db.models import F
 
 
 class Command(BaseCommand):
@@ -10,8 +11,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         one_day_ago = timezone.now() - timedelta(days=1)
-        (Reservation.objects
-         .filter(date__lte=one_day_ago, status=StatusChoices.ACTIVE)
-         .update(status=StatusChoices.INACTIVE)
-         )
-        self.stdout.write(self.style.SUCCESS('Reservations status updated'))
+        reservations_to_update = Reservation.objects.filter(date__lte=one_day_ago, status=StatusChoices.ACTIVE)
+
+        for reservation in reservations_to_update:
+            reservation.book.quantity = F('quantity') + 1
+            reservation.book.save()
+
+        reservations_to_update.update(status=StatusChoices.INACTIVE)
+
+        self.stdout.write(self.style.SUCCESS('Reservations status updated and book quantities increased'))
--- reservations/serializers.py
+++ reservations/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+
+from library.models import Book
 from reservations.choices import StatusChoices
 from reservations.models import Reservation
 
@@ -8,6 +10,13 @@ class CreateReservationSerializer(serializers.ModelSerializer):
         book_id = self.context['book_id']
         user = self.context['request'].user
         status = validated_data.get('status', StatusChoices.ACTIVE)
+
+        book = Book.objects.get(id=book_id)
+        if book.quantity == 0:
+            raise serializers.ValidationError('Book is not available for reservation')
+        book.quantity -= 1
+        book.save()
+
         reservation = Reservation.objects.create(
             book_id=book_id,
             borrower_id=user.borrower_profile.id,
