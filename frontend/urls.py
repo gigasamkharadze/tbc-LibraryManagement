903dd342a2b406dc40deba51adbb35bf63bc7252
gigasamkharadze <samkharadze.giga@kiu.edu.ge>
2024-05-20 22:05:00 +0400

    feat: list and detail views of books

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from frontend.views import home, login_view, register, logout_view
+from frontend.views import home, login_view, register, logout_view, books, book
 
 
 urlpatterns = [
@@ -7,4 +7,6 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('register/', register, name='register-frontend'),
     path('logout/', logout_view, name='logout'),
+    path('books/', books, name='books'),
+    path('books/<int:book_id>/', book, name='book'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -62,3 +62,51 @@ def logout_view(request):
         del request.session['refresh']
     logout(request)
     return redirect('home')
+
+
+def books(request):
+    if 'access' in request.session:
+        headers = {'Authorization': f'Bearer {request.session["access"]}'}
+        try:
+            response = requests.get('http://localhost:8000/api/library/books/', headers=headers)
+            response.raise_for_status()
+        except requests.exceptions.RequestException:
+            logout(request)
+            return render(request, 'frontend/error.html', {
+                'message': 'An error occurred while fetching books.'
+            })
+
+        if response.status_code == 200:
+            all_books = response.json()['results']
+        else:
+            all_books = []
+        return render(request, 'frontend/books.html', {
+            'user': request.user,
+            'books': all_books,
+        })
+    else:
+        return redirect('login')
+
+
+def book(request, book_id):
+    if 'access' in request.session:
+        headers = {'Authorization': f'Bearer {request.session["access"]}'}
+        try:
+            response = requests.get(f'http://localhost:8000/api/library/books/{book_id}/', headers=headers)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            logout(request)
+            return render(request, 'frontend/error.html', {
+                'message': 'An error occurred while fetching the book.'
+            })
+
+        if response.status_code == 200:
+            book_detail = response.json()
+        else:
+            book_detail = {}
+        return render(request, 'frontend/book.html', {
+            'user': request.user,
+            'book': book_detail,
+        })
+    else:
+        return redirect('login')
